49501a5140e642c201e79b9bd4da6d355bdbf124
Kristen Trzonkowski <kristen.trzonkowski@epicslantpress.com>
2017-09-13 07:32:03 -0400

    Doing some reorganization because I was dumb.

---
+++ samples/functions/emoji.js
@@ -0,0 +1,23 @@
+module.exports = {
+  command: {
+    "name": "#emoji",
+    "desc": "A list of all Q&B Server emoji.",
+    "group": "General",
+    "prompts": [
+      "#emoji"
+    ],
+    "role": "Cadets",
+    "channels": [
+      "All"
+    ]
+  },
+  execute: function(bot, args, message) {
+    var output = "";
+
+    bot.server.emojis.map(function(emoji) {
+      output += emoji.toString() + " :" + emoji.name + ": \r\n";
+    });
+
+    message.channel.sendMessage(output).catch(console.error);
+  }
+}
